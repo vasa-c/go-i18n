e6946d093b79a62b737b3f684ec6a3ee32b199f3
Grigoriev Oleg <go.vasac@gmail.com>
2013-10-04 19:16:28 +0400

    Helpers\ConfigNormalizer::languagesNormalize(): Normalize the list of languages

---
+++ src/Helpers/ConfigNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The normalizer configuration
+ *
+ * @package go\I18n
+ * @author Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\I18n\Helpers;
+
+class ConfigNormalizer
+{
+    /**
+     * Normalize the list of languages
+     *
+     * @param array $languages
+     *        the list from config
+     * @param string $default [optional]
+     *        the default language
+     * @return array
+     *         the languages list in normal form
+     */
+    public static function languagesNormalize(array $languages, $default = null)
+    {
+        foreach ($languages as $key => &$params) {
+            if (\is_array($params)) {
+                if (!isset($params['title'])) {
+                    $params['title'] = $key;
+                }
+                if (!\array_key_exists('parent', $params)) {
+                    $params['parent'] = ($key === $default) ? null : $default;
+                }
+                if (!isset($params['url'])) {
+                    $params['url'] = $key;
+                }
+            } else {
+                $parent = \is_string($params) ? $params : $default;
+                $params = array(
+                    'title' => $key,
+                    'parent' => $parent,
+                    'url' => $key,
+                );
+            }
+        }
+        return $languages;
+    }
+}
---
+++ tests/Helpers/ConfigNormalizerTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Test of the config normalize helper
+ *
+ * @package go\I18n
+ * @subpackage Tests
+ * @author Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\Tests\I18n\Helpers;
+
+use go\I18n\Helpers\ConfigNormalizer;
+
+/**
+ * @covers go\I18n\Helpers\ConfigNormalizer
+ */
+class ConfigNormalizerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers go\I18n\Helpers\ConfigNormalizer::languageNormalize
+     */
+    public function testLanguageNormalize()
+    {
+        $languages = array(
+            'en' => array(
+                'title' => 'English',
+                'url' => 'en',
+            ),
+            'ru' => array(
+                'url' => 'rus',
+            ),
+            'it' => true,
+            'jp' => true,
+            'by' => 'ru',
+            'ua' => array(
+                'title' => 'Ukranian',
+                'parent' => 'ru',
+            ),
+        );
+        $expected = array(
+            'en' => array(
+                'title' => 'English',
+                'parent' => null,
+                'url' => 'en',
+            ),
+            'ru' => array(
+                'title' => 'ru',
+                'parent' => 'en',
+                'url' => 'rus',
+            ),
+            'it' => array(
+                'title' => 'it',
+                'parent' => 'en',
+                'url' => 'it',
+            ),
+            'jp' => array(
+                'title' => 'jp',
+                'parent' => 'en',
+                'url' => 'jp',
+            ),
+            'by' => array(
+                'title' => 'by',
+                'parent' => 'ru',
+                'url' => 'by',
+            ),
+            'ua' => array(
+                'title' => 'Ukranian',
+                'parent' => 'ru',
+                'url' => 'ua',
+            ),
+        );
+        $actual = ConfigNormalizer::languagesNormalize($languages, 'en');
+        $this->assertEquals($expected, $actual);
+    }
+}
