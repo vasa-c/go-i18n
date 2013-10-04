1a3ea7e26275e8ea4ea642e463ce3956ef3a1fac
Grigoriev Oleg <go.vasac@gmail.com>
2013-10-04 18:56:40 +0400

    Basic exceptions

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The basic exception of library
+ *
+ * @package go\I18n
+ * @author Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\I18n\Exceptions;
+
+interface Exception
+{
+}
---
+++ src/Exceptions/Logic.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The basic class of library logic-exceptions
+ *
+ * @package go\I18n
+ * @author Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\I18n\Exceptions;
+
+abstract class Logic extends \LogicException implements Exception
+{
+    /**
+     * Constructor
+     *
+     * @param string $message
+     */
+    public function __construct($message = null)
+    {
+        $message = $message ?: $this->errorMessage;
+        parent::__construct($message);
+    }
+
+    /**
+     * Error message by default
+     *
+     * @var string
+     */
+    protected $errorMessage = 'Logic exception';
+}
